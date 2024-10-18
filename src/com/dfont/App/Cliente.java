f7c09dff5f3c80ad918a96a7afb0ad50e53519fe
DavidFontainaAquino <david.fontaina.w@gmail.com>
2024-10-18 18:22:04 -0300

    Se agregan los comentarios necesarios

--- src/com/dfont/App/Cliente.java
+++ src/com/dfont/App/Cliente.java
@@ -12,31 +12,44 @@ import java.util.logging.Logger;
 
 public class Cliente {
 	public static void main(String[] args) {
-
+		
+		
 		final int PUERTO_SERVIDOR = 5000;
 		byte[] buffer = new byte[1024];
 		Logger logger = Logger.getLogger(Cliente.class.getName());
 		String mensaje = new String("Hola servidor, soy el cliente");
 		buffer = mensaje.getBytes(StandardCharsets.UTF_8);
-		System.out.println(buffer);
 		
 		try {
 			InetAddress direccionServidor = InetAddress.getByName("localhost");
+			
+			// Se crea el socket para la conexion UDP
 			DatagramSocket socket = new DatagramSocket();
 			logger.log(Level.INFO, "Socket creado" );
+			
+			// Se crea el DatagramPacket que tiene la dirección y puerto del servidor
 			DatagramPacket solicitud = new DatagramPacket(buffer, buffer.length, direccionServidor, PUERTO_SERVIDOR);
+			
+			// Se envia el datagrama a travez del socket
 			socket.send(solicitud);
 			logger.log(Level.INFO, "Solicitud enviada");
 			
+			// Se crea un nuevo DatagramPacket para recibir la respuesta del servidor
 			DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length);
+			
+			// Se pone el socket a la espera de una respuesta que sera almacenado en "respuesta"
+			// En este lugar el aplicativo quedara a la espera de una comunicacion
 			socket.receive(respuesta);
 			logger.log(Level.INFO, "Respuesta recibida");
+			
+			// Luego de recibir el mensaje se convierte el contenido del buffer en un String
 			mensaje= new String(buffer, StandardCharsets.UTF_8);
 			logger.log(Level.INFO, "Recibido: " + mensaje);
+			
+			// Se cierra el socket
 			socket.close();
 
 		} catch (SocketException e) {
-			
 			logger.log(Level.SEVERE,"Ocurrio una excepcion en la creacion del socket", e);
 		} catch (UnknownHostException e) {
 			logger.log(Level.SEVERE, "Ocurrio una excepcion en la resolucion del host", e);
--- src/com/dfont/App/Servidor.java
+++ src/com/dfont/App/Servidor.java
@@ -17,23 +17,37 @@ public class Servidor {
 		Logger logger = Logger.getLogger(Servidor.class.getName()); 
 		DatagramSocket socket;
 		try {
+			// Se solicita al SO la apertura de un socket en el puerto especificado.
 			socket = new DatagramSocket(PUERTO);
 			logger.log(Level.INFO, "Servidor Iniciado");
-
+			
+			// Se crea un DatagramPacket donde se almacenara la peticion del cliente.
 			DatagramPacket peticion = new DatagramPacket(buffer, buffer.length);
+			
+			// El aplicativo queda a la espera de una peticion de algun cliente
 			socket.receive(peticion);
 			logger.log(Level.INFO, "Esperando peticion");
-
+			
+			// Luego de recibir el mensaje se convierte el contenido del buffer en un String
 			String mensaje = new String(buffer, StandardCharsets.UTF_8);
 			logger.log(Level.INFO, "Recibido: " + mensaje);
 			
+			// Se obtiene la direccion IP y puerto del cliente
 			int puertoCliente = peticion.getPort();
 			InetAddress direccionCliente = peticion.getAddress();
+			
+			// Se crea el mensaje de respuesta y se almacena en el buffer
 			mensaje = "Hola Cliente, soy el servidor";
 			buffer = mensaje.getBytes();
+			
+			// Se crea el DatagramPacket que se enviara al cliente
 			DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length, direccionCliente, puertoCliente);
+			
+			// Se envia la respuesta al cliente a travez de el socket creado
 			socket.send(respuesta);
 			logger.log(Level.INFO, "Respuesta enviada.");
+			
+			// Se cierra el socket
 			socket.close();
 			
 		} catch (SocketException e) {
@@ -42,5 +56,4 @@ public class Servidor {
 			logger.log(Level.SEVERE, "Ocurrio una excepcion en el envio o recepcion del datagramas", e);
 		}
 	}
-
 }
