3e1a466f7d93e9eddb6dbd0b40c9cafc7cf86088
DavidFontainaAquino <david.fontaina.w@gmail.com>
2024-10-19 02:49:48 -0300

    Se agrega comprobacion de existencia de java en el script

--- ejecutar.sh
+++ ejecutar.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-clear
-dir=$(pwd)/src/com/dfont/App
+if ! command -v jav --version 2>&1 > /dev/null; 
+	then
+		echo -e "\033[31mNo se encuentra java en su PC"
+	else 
+		clear
+		dir=$(pwd)/src/com/dfont/App
 
-# Se lanza el servidor
-java "$dir"/Servidor.java &
+		# Se lanza el servidor
+		java "$dir"/Servidor.java &
 
-# Se lanza el cliente
-java "$dir"/Cliente.java &
+		# Se lanza el cliente
+		java "$dir"/Cliente.java &
+fi
